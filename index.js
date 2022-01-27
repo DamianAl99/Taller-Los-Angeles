f4760293ff05ca6d36853d1a52c232696fd5119a
Damian <damiianalmada06@gmail.com>
2022-01-27 19:24:17 -0300

    nuevos recursos

--- index.js
+++ index.js
@@ -21,21 +21,11 @@ var hola = async (page)=>{
    // const appDiv = document.getElementById('app');
     console.log("hola");
     await page.goto('https://www.instagram.com/leomessi/?__a=1')
-    await page.screenshot({path: "img.png"})
-    var aa = await page.locator("/html/body/pre/text()[2]").text
+    //await page.screenshot({path: "img.png"})
+    //var aa = await page.locator("/html/body/pre/text()[2]").text
     const text = await page.innerText('body>pre');
     var textJson = JSON.parse(text)
-    fs.writeFile("./test.txt", text, function (err) {
-        // la funcion es la que maneja lo que sucede despues de termine el evento
-        if (err) {
-            return console.log(err);
-        }
-        // las funciones de javascript en nodejs son asincronicas
-        // por lo tanto lo que se quiera hacer debe hacerse dentro de la funcion que maneja el evento
-        // si uno declara una variable arriba de la funcion, la manipula dentro y la quiere usar
-        // despues afuera, se corre el riezgo de que nunca se realice la manipulacion.
-        console.log("The file was saved!");
-    });
+    
     textJson.graphql.user.edge_owner_to_timeline_media.edges.map(item => console.log(item.node.display_url))
 };
 
