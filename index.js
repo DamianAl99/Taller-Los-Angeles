3fc5f5350d861961ca0c635168c879faf0425e37
Damian <damiianalmada06@gmail.com>
2022-01-29 02:11:50 -0300

    asdf

--- index.js
+++ index.js
@@ -27,62 +27,40 @@
 
 const express = require("express")
 const { chromium } = require("playwright-chromium")
-
 const app = express()
 //app.use(express.static("./public"))
 const port = process.env.PORT || 3000;
+const engines = ['chromium', 'firefox', 'webkit'];
+app.get('/', (req, res) => {
+  res.send(`
+  <a href="/test/chromium">Test Chromium</a>
+  `);
+});
 
-var a = async () => {
-  const browserName = "chromium"
-  if (!["chromium"].includes(browserName)) {
-    return res.status(500).send(`invalid browser name (${browserName})!`)
+app.get('/test/:browserName', async (req, res) => {
+  const name = req.params.browserName;
+  console.log(`Testing browser engine: ${name}`);
+  if (!engines.includes(name)) {
+    return res.send(`Not available engine ${name}`);
   }
-  const url = "https://www.instagram.com/accounts/login/"
-  const waitUntil = "load"
-  const width =  1920
-  const height = 1080
-  console.log(`Incoming request for browser '${browserName}' and URL '${url}'`)
+
   try {
-    /** @type {import('playwright-chromium').Browser} */
-    const browser = await { chromium }[browserName].launch({
-      //headless: true
-        chromiumSandbox: false
+    const browser = await { chromium }[name].launch({
+              chromiumSandbox: false
     })
-    const page = await browser.newPage({
-      viewport: {
-        width,
-        height
-      }
-    })
-    await page.goto(url, {
-      timeout: 10 * 1000,
-      waitUntil
-    })
-    await page.fill('input[name="username"]', process.env.USER)
-    await page.waitForTimeout(2000)
-    await page.fill('input[name="password"]', process.env.PASS)
-    await page.waitForTimeout(2000)
-    await page.screenshot({path: "./imga.png"})
-    await page.locator("#loginForm>div>div:nth-child(3)>button>div").click()
-    await page.waitForTimeout(5000)
-    //await page.screenshot({path: "./imgb.png"})
-    await page.goto('https://www.instagram.com/leomessi/?__a=1', {timeout: 10 * 1000,
-    waitUntil})
-    await page.waitForTimeout(2000)
-    //await page.screenshot({path: "./imgc.png"})
-    const text = await page.innerText('body>pre');
-    var textJson = JSON.parse(text)
-    var data = ""
-    textJson.graphql.user.edge_owner_to_timeline_media.edges.map(item => data += item.node.display_url)
-    await browser.close()
-    console.log(data)
-  } catch (err) {
-    console.log(err)
-    //res.status(500).send(`Something went wrong: ${err}`)
+    const context = await browser.newContext();
+    const page = await context.newPage('http://whatsmyuseragent.org/');
+    
+    await page.screenshot({ path: `example-${name}.png` });
+    await browser.close();
+    console.log(`Testing ${name} successfull!`);
+    res.send('DONE');
+  } catch(err) {
+    console.log(err);
+    res.send(err + '');
   }
-};
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
-  a()
 });
