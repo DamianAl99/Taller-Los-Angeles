1df213d51238a0e64676c312b9fbfc2bd8550a1a
Damian <damiianalmada06@gmail.com>
2022-02-22 22:26:02 -0300

    div proyectos

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@ let btnclose = document.querySelector(".menu-icon-close");
 let divMenu = document.querySelector("#menu");
 let divServicios = document.querySelector(".super-item");
 let divProyectos = document.querySelector(".proyectos-items");
+let divPagesProyectos = document.querySelector(".pagesProyectos-items");
+
+let btnProyectos = document.querySelector(".btn-proyectos");
+let btnVolver = document.querySelector("#Volver");
+let btnEngranage = document.querySelector(".divEngranage");
 
 const clickMenu = () => {
   btnMenu.addEventListener("click", () => {
@@ -16,6 +21,21 @@ const clickMenu = () => {
     document.querySelector("header").classList.toggle("header-open");
   });
 };
+
+const clickProyectos = () => {
+  btnProyectos.addEventListener("click", ()=>{
+    document.querySelector("#pagesProyectos").classList.toggle("mostrarProyectos")
+    document.querySelector(".main").classList.toggle("ocultarMain")
+  })
+  btnVolver.addEventListener("click", ()=>{
+    document.querySelector("#pagesProyectos").classList.toggle("mostrarProyectos")
+    document.querySelector(".main").classList.toggle("ocultarMain")
+  })
+  btnEngranage.addEventListener("click", ()=>{
+    document.querySelector("#pagesProyectos").classList.toggle("mostrarProyectos")
+    document.querySelector(".main").classList.toggle("ocultarMain")
+  })
+}
 
 const ServiciosJson = [
   {
@@ -36,19 +56,58 @@ const ServiciosJson = [
 ];
 
 const ProyectosJson = [
-    {
-        title : "Alineacion de ruedas",
-        img : "images/img-a.jpg"
-    },
-    {
-        title : "Alineacion de ruedas",
-        img : "images/img-b.jpg"
-    },
-    {
-        title : "Alineacion de ruedas",
-        img : "images/img-c.jpg"
-    },
-]
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-a.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-b.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-c.jpg",
+  },
+];
+
+const PagesProyectosJson = [
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-a.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-b.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-c.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-a.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-b.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-c.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-a.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-b.jpg",
+  },
+  {
+    title: "Alineacion de ruedas",
+    img: "images/img-c.jpg",
+  },
+];
 
 const pintarServiciosJson = () => {
   let content = "";
@@ -65,33 +124,27 @@ const pintarServiciosJson = () => {
 };
 
 const pintarProyectosJson = () => {
-    let content = "";
-    ProyectosJson.forEach((proyectos) => {
-      content += `
+  let content = "";
+  ProyectosJson.forEach((proyectos) => {
+    content += `
       <div class="title">${proyectos.title}</div>
-      <div class="item"><img loading="lazy"src="${proyectos.img}" alt="" srcset=""></div>`;
-    });
-    divProyectos.innerHTML = content;
-  };
+      <div class="item"><img loading="lazy  "src="${proyectos.img}" alt="" srcset=""></div>`;
+  });
+  divProyectos.innerHTML = content;
+};
+
+const pintarPagesProyectosJson = () => {
+  let content = "";
+  PagesProyectosJson.forEach((proyectos) => {
+    content += `
+      <div class="title">${proyectos.title}</div>
+      <div class="item"><img loading="lazy  "src="${proyectos.img}" alt="" srcset=""></div>`;
+  });
+  divPagesProyectos.innerHTML = content;
+};
 
 pintarServiciosJson();
 pintarProyectosJson();
+pintarPagesProyectosJson();
 clickMenu();
-/* <div class="item-servicios">
-<div class="title">Alineacion de ruedas</div>
-<div class="description">
-    <p>contamos con el servicio de alineación de ruedas, realizado con un equipo y materiales sin precedentes para proporcionarle a su vehículo las máximas garantías. Trabajamos con la marca Space, que nos proporciona lo último en alineadoras de ruedas, unos dispositivos sin precedentes y que hacen del trabajo un proceso mucho más eficiente y profesional</p>
-</div>
-</div>
-<div class="item-servicios">
-<div class="title">Alineacion de ruedas</div>
-<div class="description">
-    <p>contamos con el servicio de alineación de ruedas, realizado con un equipo y materiales sin precedentes para proporcionarle a su vehículo las máximas garantías. Trabajamos con la marca Space, que nos proporciona lo último en alineadoras de ruedas, unos dispositivos sin precedentes y que hacen del trabajo un proceso mucho más eficiente y profesional</p>
-</div>
-</div>
-<div class="item-servicios">
-<div class="title">Alineacion de ruedas</div>
-<div class="description">
-    <p>contamos con el servicio de alineación de ruedas, realizado con un equipo y materiales sin precedentes para proporcionarle a su vehículo las máximas garantías. Trabajamos con la marca Space, que nos proporciona lo último en alineadoras de ruedas, unos dispositivos sin precedentes y que hacen del trabajo un proceso mucho más eficiente y profesional</p>
-</div>
-</div> */
+clickProyectos();
